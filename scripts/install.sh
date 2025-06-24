f142f95669cd30753a03d4a381ab125e3c55cd26
Suriyaa MM <suriyaamm.2705@gmail.com>
2025-06-24 20:34:56 +0530

    dockerfile support check (not implemented yet)

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,2 @@
-echo "creating conda environment"
-conda create env -n synapse-works python=3.12 && conda update env -f environment.yaml
-
-echo "activating conda environment"
-conda activate synapse-works
-
 echo "installing node modules"
 npm install && cd frontend && npm install
