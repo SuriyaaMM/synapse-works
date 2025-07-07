8e6363a4e17e23e84e6dd9c7da057f324602bc23
dhanya180 <cs23b055@iittp.ac.in>
2025-07-07 12:17:28 +0000

    changed the graphql endpoint

--- frontend/src/lib/apolloClient.ts
+++ frontend/src/lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 
 const client = new ApolloClient({
-  uri: 'https://synapse-works-server.onrender.com/graphql',
+  uri: 'https://synapse-works-server.onrender.com/',
   cache: new InMemoryCache(),
 });
 
