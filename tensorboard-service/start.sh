f59fa51fe6025346df09ff7e4f6bbe6160389605
dhanya180 <cs23b055@iittp.ac.in>
2025-07-09 07:31:42 +0000

    changed start.sh

--- tensorboard-service/start.sh
+++ tensorboard-service/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Start TensorBoard on Render's only available port: 10000
+# Install Python dependencies (if needed)
+pip install --no-cache-dir tensorboard
+
+# Start TensorBoard on Render's public port (10000)
 tensorboard --logdir ../tbsummary --host 0.0.0.0 --port 10000
